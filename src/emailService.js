0d0b30e59172e1ee098d91a99b02973577892563
Sujit <mnamesujit@gmail.com>
2023-12-21 21:34:05 +0530

    nodemon setup and added comments

--- src/emailService.js
+++ src/emailService.js
@@ -1,103 +1,3 @@
-// const fs = require("fs");
-// const dotenv = require("dotenv");
-// dotenv.config();
-//
-// async function processEmails(gmailService) {
-//   const gmail = await gmailService.authorize();
-//   const messages = await getNewEmails(gmail);
-//
-//   for (const message of messages) {
-//     const threadId = message.threadId;
-//     const hasReplied = await hasRepliedToThread(gmail, threadId);
-//
-//     if (!hasReplied) {
-//       const label = await createLabel(gmail, "VacationReply");
-//       await sendReply(gmail, message.id, label.id);
-//     }
-//   }
-// }
-//
-// async function getNewEmails(gmail) {
-//   const response = await gmail.users.messages.list({
-//     userId: process.env.GMAIL_USER,
-//     q: "is:inbox -label:VacationReply", // Exclude already processed emails
-//   });
-//
-//   return response.data.messages || [];
-// }
-//
-// async function hasRepliedToThread(gmail, threadId) {
-//   const response = await gmail.users.messages.list({
-//     userId: process.env.GMAIL_USER,
-//     q: `in:inbox thread:${threadId} from:me`,
-//   });
-//
-//   return response.data.messages.length > 0;
-// }
-//
-// async function sendReply(gmail, messageId, labelId) {
-//   const message = await gmail.users.messages.get({
-//     userId: process.env.GMAIL_USER,
-//     id: messageId,
-//   });
-//
-//   const reply = `Thank you for your email! I'm currently out of the office and will get back to you soon.`;
-//
-//   await gmail.users.messages.send({
-//     userId: process.env.GMAIL_USER,
-//     requestBody: {
-//       threadId: message.data.threadId,
-//       labelIds: [labelId],
-//       raw: Buffer.from(
-//         createMimeMessage(
-//           process.env.GMAIL_USER,
-//           message.data.payload.headers[0].value,
-//           reply,
-//         ),
-//       ).toString("base64"),
-//     },
-//   });
-// }
-//
-// async function createLabel(gmail, labelName) {
-//   const labels = await gmail.users.labels.list({
-//     userId: process.env.GMAIL_USER,
-//   });
-//
-//   const existingLabel = labels.data.labels.find(
-//     (label) => label.name === labelName,
-//   );
-//
-//   if (existingLabel) {
-//     return existingLabel;
-//   } else {
-//     const response = await gmail.users.labels.create({
-//       userId: process.env.GMAIL_USER,
-//       requestBody: {
-//         name: labelName,
-//       },
-//     });
-//
-//     return response.data;
-//   }
-// }
-//
-// function createMimeMessage(sender, to, body) {
-//   const messageParts = [
-//     `From: ${sender}`,
-//     `To: ${to}`,
-//     "Content-Type: text/plain; charset=UTF-8",
-//     "",
-//     body,
-//   ];
-//
-//   return messageParts.join("\n");
-// }
-//
-// module.exports = { processEmails };
-//
-
-// emailService.js
 
 const { google } = require('googleapis');
 const { authorize } = require('./gmail');
@@ -109,7 +9,7 @@ async function processEmails() {
 
     const response = await gmail.users.messages.list({
       userId: 'me',
-      labelIds: ['INBOX'], // Adjust the label if needed
+      labelIds: ['INBOX'],
     });
 
     if (!response || !response.data || !response.data.messages) {
@@ -125,13 +25,13 @@ async function processEmails() {
 
         if (!messageDetails || !messageDetails.payload) {
           console.error('Invalid message details. Cannot send reply.');
-          continue; // Skip to the next message
+          continue; // Skiping to the next message
         }
-
-        // Check if a reply has been sent
+        console.log(messageDetails)
+        // Checking if a reply has been sent
         if (!messageDetails.payload.headers.some(header => header.name === 'In-Reply-To')) {
-          // Send a sample reply
-          const replyContent = 'Thank you for your email. I am currently out of the Internet Access and will respond as soon as possible.';
+          // Sending a sample reply
+          const replyContent = 'Hi there, I am currently out of the Space and will respond as soon as possible.';
           await sendReply(gmail, messageDetails.threadId, replyContent, messageDetails);
         }
       } catch (error) {
@@ -155,42 +55,127 @@ async function getMessageDetails(gmail, messageId) {
   return message.data;
 }
 
-async function sendReply(gmail, threadId, replyContent, messageDetails) {
-  if (!messageDetails || !messageDetails.payload) {
-    console.error('Invalid message details. Cannot send reply.');
-    return;
-  }
 
-  const fromHeader = messageDetails.payload.headers.find(header => header.name === 'From');
-  const subjectHeader = messageDetails.payload.headers.find(header => header.name === 'Subject');
+async function sendReply(gmail, threadId,replyContent ,messageDetails) {
+  try {
+    if (!messageDetails || !messageDetails.payload || !messageDetails.payload.headers) {
+      console.error('Invalid message details. Cannot send reply.');
+      return;
+    }
 
-  if (!fromHeader || !subjectHeader) {
-    console.error('Required headers not found. Cannot send reply.');
-    return;
-  }
+    const fromHeader = messageDetails.payload.headers.find(header => header.name === 'From');
+    const subjectHeader = messageDetails.payload.headers.find(header => header.name === 'Subject');
 
+    if (!fromHeader || !subjectHeader) {
+      console.error('Required headers not found. Cannot send reply.');
+      return;
+    }
+
+    // Checking if a reply has been sent
+    const replySentHeader = messageDetails.payload.headers.find(header => header.name === 'X-Auto-Reply-Sent');
+    if (replySentHeader && replySentHeader.value === 'true') {
+      console.log('Auto-reply already sent. Skipping.');
+      return;
+    }
+
+    // Marking the message as replied to avoid repeated replies
+    await markMessageAsReplied(gmail, messageDetails.id);
+
+    // Create a HTML email template
+    const htmlContent = `
+    <html>
+      <head>
+        <style>
+          body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f5f5f5;
+            color: #333;
+            padding: 20px;
+          }
+          .reply-container {
+            background-color: #fff;
+            border-radius: 8px;
+            padding: 15px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+          }
+          h2 {
+            color: #007bff;
+          }
+          p {
+            color: #333;
+          }
+          .polite-message {
+            color: #28a745;
+            font-weight: bold;
+          }
+          .details-container {
+            margin-top: 20px;
+            padding: 15px;
+            border: 1px solid #ddd;
+            border-radius: 8px;
+            background-color: #f9f9f9;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="reply-container">
+          <h2>I'm Extremely Sorry for the Inconvenience</h2>
+          <p>${replyContent}</p>
+          <p class="polite-message">Thank you for your email. I sincerely appreciate your message.</p>
+          <div class="details-container">
+            <p><strong>If it's emergency, please reach out to me through [+91-9305083852].</strong></p>
+            <ul>
+              <li>I apologize for any delay in my response.</li>
+              <li>Your understanding is highly appreciated.</li>
+              <li>Please feel free to reach out for any further assistance.</li>
+            </ul>
+          </div>
+        </div>
+      </body>
+    </html>
+  `;
+  console.log(fromHeader.value)
   const raw = Buffer.from(
     `To: ${fromHeader.value}\r\n` +
-    `Subject: Re: ${subjectHeader.value}\r\n` +
+    `Subject: Re[Bot]: ${subjectHeader.value}\r\n` +
+    'Content-Type: text/html; charset="UTF-8"\r\n' +
     '\r\n' +
-    `${replyContent}\r\n`
+    `${htmlContent}\r\n`
   ).toString('base64');
 
-  // Send the reply
-  try {
-    await gmail.users.messages.send({
-      userId: 'me',
-      requestBody: {
-        raw,
-        threadId,
-      },
-    });
+    // Sending the reply
+    try {
+      await gmail.users.messages.send({
+        userId: 'me',
+        requestBody: {
+          raw,
+          threadId,
+        },
+      });
 
-    console.log('Reply sent successfully.');
+      console.log('Reply sent successfully.');
+    } catch (error) {
+      console.error('Error sending reply:', error.message);
+    }
   } catch (error) {
-    console.error('Error sending reply:', error.message);
+    console.error('Error processing message details:', error.message);
   }
 }
 
+
+async function markMessageAsReplied(gmail, messageId) {
+  // Add a custom header to mark the message as replied
+  await gmail.users.messages.modify({
+    userId: 'me',
+    id: messageId,
+    requestBody: {
+      addLabelIds: ['INBOX'], // You can adjust the label if needed
+      removeLabelIds: ['UNREAD'], // Remove the unread label to mark it as read
+      headers: {
+        'X-Auto-Reply-Sent': 'true',
+      },
+    },
+  });
+}
 
 module.exports = { processEmails };
